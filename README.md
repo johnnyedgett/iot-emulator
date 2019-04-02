5e7d4b2d206ec06b68b3a4c83a3a183b85fc9812
johnnyedgett <bostonjohn94@gmail.com>
2019-04-02 12:52:50 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Bootstrapped with expo CLI!
+
+Nothing to add yet - will update the readme as this progresses.
+
+The goal is to have an app for your phone to emulate different types of IoT sensors. It should be able to connect to some mqtt server to send and receive messages. I'm creating this to support some side projects I am working on.
---
+++ screens/NewDeviceScreen.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Button, View, StyleSheet } from 'react-native'
+import t from 'tcomb-form-native'
+const Form = t.form.Form;
+const Device = t.struct({
+    'Device Name': t.String,
+    'Hostname': t.String,
+    'Port': t.String,
+    'Subscribe to':t.String,
+    'Publish to':t.String,
+  });
+
+export default class NewDeviceScreen extends React.Component {
+    static navigationOptions = {
+        title: 'New Device',
+    };
+    handleSubmit = () => {
+        const formValue = this._form.getValue();
+        console.log(formValue)
+    }
+    render(){
+        return (
+            <View style={styles.container}>
+                <Form 
+                    type={Device}
+                    ref={c => this._form = c}
+                    />
+                <Button
+                    title="Add New Device"
+                    onPress={this.handleSubmit}
+                    />
+            </View>
+        )
+
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        marginLeft: 10,
+        marginRight: 10
+        // flex: 1,
+        // justifyContent: 'center',
+        // alignItems: 'center'
+    }
+})
